488607bfb5cdab2b760eac32196b13c08b93103a
Riccardo Binetti <rbino@gmx.com>
2023-08-28 21:42:25 +0200

    Add NimbleParsec to the deps

--- mix.exs
+++ mix.exs
@@ -49,7 +49,8 @@ defmodule TudeeFinder.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:nimble_parsec, "~> 1.3"}
     ]
   end
 
