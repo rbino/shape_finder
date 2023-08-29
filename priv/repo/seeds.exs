e6428ce96b5563158018ab05f65a601de17dfb29
Riccardo Binetti <rbino@gmx.com>
2023-08-30 01:27:37 +0200

    Add Tudees in seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,17 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias TudeeFinder.Tudees
+alias TudeeFinder.Tudees.Tudee
+
+Range.new(0, 20)
+|> Enum.each(fn _ ->
+  attrs = %{
+    dimensions: Enum.random(Ecto.Enum.values(Tudee, :dimensions)),
+    color: Enum.random(Ecto.Enum.values(Tudee, :color)),
+    sides: Enum.random(3..9)
+  }
+
+  {:ok, _} = Tudees.create_tudee(attrs)
+end)
