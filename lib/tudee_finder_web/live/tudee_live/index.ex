f06b8ec3e092cf35648db1733bab3f54aea41512
Riccardo Binetti <rbino@gmx.com>
2023-08-30 00:51:57 +0200

    Allow writing a selector for the Tudee list in LiveView

--- lib/tudee_finder_web/live/tudee_live/index.ex
+++ lib/tudee_finder_web/live/tudee_live/index.ex
@@ -1,14 +1,22 @@
 defmodule TudeeFinderWeb.TudeeLive.Index do
   use TudeeFinderWeb, :live_view
 
+  alias TudeeFinder.Selector
+  alias TudeeFinder.Selector.Parser
   alias TudeeFinder.Tudees
   alias TudeeFinder.Tudees.Tudee
 
   import TudeeFinderWeb.TudeeLive.TudeeComponent
 
   @impl true
-  def mount(_params, _session, socket) do
-    {:ok, stream(socket, :tudees, Tudees.list_tudees())}
+  def mount(params, _session, socket) do
+    socket =
+      socket
+      |> assign(:selector, nil)
+      |> assign(:selector_error, nil)
+      |> stream_filtered_tudees(params["selector"])
+
+    {:ok, socket}
   end
 
   @impl true
@@ -45,5 +53,47 @@ defmodule TudeeFinderWeb.TudeeLive.Index do
     {:ok, _} = Tudees.delete_tudee(tudee)
 
     {:noreply, stream_delete(socket, :tudees, tudee)}
+  end
+
+  @impl true
+  def handle_event("selector_updated", %{"selector" => selector}, socket) do
+    {:noreply, stream_filtered_tudees(socket, selector)}
+  end
+
+  defp stream_filtered_tudees(socket, "") do
+    stream_filtered_tudees(socket, nil)
+  end
+
+  defp stream_filtered_tudees(socket, nil) do
+    socket
+    |> assign(:selector, nil)
+    |> assign(:selector_error, nil)
+    |> assign(:where, nil)
+    |> stream(:tudees, Tudees.list_tudees(), reset: true)
+  end
+
+  defp stream_filtered_tudees(socket, selector) do
+    case Selector.where(selector) do
+      {:ok, where} ->
+        socket
+        |> assign(:selector, selector)
+        |> assign(:selector_error, nil)
+        |> assign(:where, where)
+        |> stream(:tudees, Tudees.list_tudees(where: where), reset: true)
+
+      {:error, %Parser.Error{message: message}} ->
+        # We might hit this parse error on mount, in that case show a flash and just drop
+        # the quey parameterr so we stream the unfiltered list
+        if socket.assigns[:streams][:tudees] == nil do
+          socket
+          |> put_flash(:error, "Invalid selector passed as parameter")
+          |> push_navigate(to: ~p"/tudees")
+        else
+          # TODO: provide some more detailed feedback in the input field
+          socket
+          |> assign(:selector, selector)
+          |> assign(:selector_error, message)
+        end
+    end
   end
 end
