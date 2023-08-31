8a5ef9fd2b44baa69225b85813e87392107f7a30
Riccardo Binetti <rbino@gmx.com>
2023-08-31 15:03:47 +0200

    Add SideCountFilter
    Allow filtering Tudees by their side count

--- lib/tudee_finder/selector/parser.ex
+++ lib/tudee_finder/selector/parser.ex
@@ -5,6 +5,7 @@ defmodule TudeeFinder.Selector.Parser do
 
   alias TudeeFinder.Selector.AST.ColorFilter
   alias TudeeFinder.Selector.AST.DimensionsFilter
+  alias TudeeFinder.Selector.AST.SideCountFilter
   alias TudeeFinder.Selector.Parser.Error
 
   blankspace = ignore(ascii_string([?\s, ?\n, ?\r, ?\t], min: 1))
@@ -16,6 +17,18 @@ defmodule TudeeFinder.Selector.Parser do
   chonky =
     string("chonky")
     |> replace(%DimensionsFilter{dimensions: :big})
+
+  # Order is important here: if we put > before >=, the second will never match
+  numeric_comparison_operator =
+    choice([
+      string("=="),
+      string("!="),
+      string(">="),
+      string(">"),
+      string("<="),
+      string("<")
+    ])
+    |> map({String, :to_existing_atom, []})
 
   dimensions =
     choice([
@@ -33,6 +46,15 @@ defmodule TudeeFinder.Selector.Parser do
     end)
     |> choice()
 
+  side_count_filter =
+    ignore(string("sides"))
+    |> optional(blankspace)
+    |> concat(numeric_comparison_operator)
+    |> optional(blankspace)
+    |> integer(min: 1)
+    |> label("side count filter")
+    |> post_traverse(:build_side_count_filter)
+
   color_filter =
     colors
     |> label("color filter")
@@ -45,12 +67,17 @@ defmodule TudeeFinder.Selector.Parser do
     optional(blankspace)
     |> choice([
       dimensions_filter,
-      color_filter
+      color_filter,
+      side_count_filter
     ])
     |> optional(blankspace)
     |> eos()
 
   defparsec(:parse_selector, tudee_selector)
+
+  defp build_side_count_filter(rest, [value, operator], context, _line, _offset) do
+    {rest, [%SideCountFilter{operator: operator, value: value}], context}
+  end
 
   def parse(selector) do
     case parse_selector(selector) do
